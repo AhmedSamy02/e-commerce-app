dd198cbc7c5bd3f0ecf3a4daf2cb1323ec8cfce2
AhmedSamy02 <nname1858@gmail.com>
2024-08-09 18:54:07 +0300

    [🐞 BUG] Adding token to profile remote data source

--- lib/features/profile/data/data_source/profile_remote_data_source.dart
+++ lib/features/profile/data/data_source/profile_remote_data_source.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:simple_ecommerce/core/constants/values.dart';
 
 abstract class ProfileRemoteDataSource {
-  Future<void>changePassword(String oldPassword, String newPassword);
-  Future<void>updateProfile(String firstName, String lastName);
+  Future<void> changePassword(String oldPassword, String newPassword);
+  Future<void> updateProfile(String firstName, String lastName);
 }
+
 class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
   final Dio dio;
 
@@ -13,17 +15,21 @@ class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
   final updateProfileEndpoint = '/profile';
   @override
   Future<void> changePassword(String oldPassword, String newPassword) async {
-    await dio.post(kMainLink + changePasswordEndpoint, data: {
-      'old_password': oldPassword,
-      'new_password': newPassword
-    });
+    final token = await getit.get<FlutterSecureStorage>().read(key: 'token');
+    await dio.post(kMainLink + changePasswordEndpoint,
+        data: {'old_password': oldPassword, 'new_password': newPassword},
+        options: Options(headers: {
+          'Authorization': token,
+        }));
   }
 
   @override
   Future<void> updateProfile(String firstName, String lastName) async {
-    await dio.post(kMainLink + updateProfileEndpoint, data: {
-      'first_name': firstName,
-      'last_name': lastName
-    });
+    final token = await getit.get<FlutterSecureStorage>().read(key: 'token');
+    await dio.post(kMainLink + updateProfileEndpoint,
+        data: {'first_name': firstName, 'last_name': lastName},
+        options: Options(headers: {
+          'Authorization': token,
+        }));
   }
 }
